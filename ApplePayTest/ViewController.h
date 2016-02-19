eb1398a1409384ee5d3b1eeb24ed57d04fc4be5c
LiSidi <1270849866@qq.com>
2016-02-19 16:23:40 +0800

    initCommit

---
+++ ApplePayTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ApplePayTest
+//
+//  Created by JoKy_Li on 16/2/19.
+//  Copyright © 2016年 Joky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <PassKit/PassKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
